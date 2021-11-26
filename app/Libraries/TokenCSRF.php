fbeeea7dc07d718d9ec25bc89fceb4b9846e161f
Genaro Martínez <gemase.tms@gmail.com>
2021-11-25 21:02:43 -0600

    Se crea la clase tokencsrf

---
+++ app/Libraries/TokenCSRF.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Libraries;
+
+class TokenCSRF {
+    /**
+     * Crea ó genera token.
+     * @return string
+     */
+    public static function creaToken() {
+        if (!Session::has('token')) {
+            $token = base64_encode(openssl_random_pseudo_bytes(32));
+            Session::add('token', $token);
+        }
+        return Session::get('token');
+    }
+
+    /**
+     * Verifica que el token exista y sea válido.
+     * @param string $token
+     * @param boolean $regenerar true: Elimina el token 
+     * enviado y genera uno nuevo.
+     * @return bool true: Token válido, false: Token no válido.
+     */
+    public static function verificaToken($token, $regenerar = true) {
+        if (Session::has('token') && Session::get('token') === $token) {
+            if ($regenerar) {
+                Session::remove('token');
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Retorna json de mensaje (alerta) cuando el token no es válido.
+     * @return mixed
+     */
+    public static function msgTokenNoValido() {
+        $aResp = [
+            'msg' => 'Token no válido, para continuar es necesario recargar la página.'
+        ];
+        return json_encode($aResp);
+    }
+}
